a1ef2e7204935c50e376613eaf0aa594d9a147df
EndCredits <alicization.han@gmail.com>
2023-02-05 15:34:13 +0800

    picasso: Copy private permissions for XiaomiParts
    Signed-off-by: EndCredits <alicization.han@gmail.com>
    Signed-off-by: itsurboimasarou <medicspam6@gmail.com>

--- device.mk
+++ device.mk
@@ -226,6 +226,9 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-parts.xml
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
